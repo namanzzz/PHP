e70aaed21ef80039238a97f2211eca19bf8088c7
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-07 03:31:02 +0530

    Update variables_and_datatypes.php

--- variables_and_datatypes.php
+++ variables_and_datatypes.php
@@ -1,1 +1,42 @@
+<?php 
 
+    $name = "whoamI";
+    $desc = "I have been a underrated coder in my college";
+    $email = "lalala@gmail.com";
+    echo $name;
+    echo "<br>";
+    echo "Hello {$name} is this your statement
+        '$desc' <br>
+        ";
+    echo " well yeah here is my email 
+    '$email'
+    ";
+
+
+
+
+    // integers
+    $age = 22;
+    $users = 2000000;
+    echo "<br> and Yeah I am $age";
+    echo "<br> shh sshhh , There are $users on my website called
+    table no 21
+    ";
+
+
+    //floats
+    $gpa = 6.88;
+    echo "<br>will never want to discuss this but Ahh so much pain that my so called college
+    gave me only $gpa you know {$gpa} gpa wtff
+    ";
+
+
+
+    //booleans
+    $wasachattu = false;
+    echo "You know why my gpa is less because 
+    I was never a..(chattu) in my life
+    "
+
+
+?>
