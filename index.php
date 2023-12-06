73833a096608bf82b2ff44a9ecb3d3b9c6bc1896
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-07 03:08:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
+<?php
+    echo"haha that was fun <br>
+    ";
+    echo "its been really tough on me"
 
+    // this is a comment well
+    /*
+        This is a multiline 
+        comment
+    */
+
+
+    
+?>
+
+<!-- The way to add html code to php program -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order a pizza</button>
+</body>
+</html>
