38e9ee92e9e4d4187a55fbfdff405cfc58be3ca8
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-09 18:10:11 +0530

    Update forLoop.php

--- Loops/forLoop.php
+++ Loops/forLoop.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a number to count to</label>
+        <input type="text" name="counter">
+        <input type="submit" value="Click here">
+
+    </form>    
+
+</body>
+</html>
+
+
+
+
+<?php
+
+// for loops: to repeat some code a certain amount of times
+
+// for($i=3000;$i>=20;$i-=1000){
+//     echo "Naman dominates <br>";
+//     echo "$i <br>";
+// }
+
+$counter = $_POST["counter"];
+for($i=1;$i<=$counter;$i++){
+    echo"$i <br>"; 
+}
+
+?>
+
+
 
