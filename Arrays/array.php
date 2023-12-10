c1989d4931e9a7c0664554449e723a7f3c80cc5d
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-10 18:35:37 +0530

    Update array.php

--- Arrays/array.php
+++ Arrays/array.php
@@ -1,1 +1,33 @@
 
+<?php
+
+    // array = "variable" which can hold more than one value at a time
+
+
+    
+    $foods = array("apple", "orange", "banana", "coconut");
+
+    $foods[0] = "Pineapple";
+
+    // echo $foods[1] . "<br>";
+    // echo $foods[3]."<br>";
+
+    array_push($foods, "mango", "kiwi");
+    // array_pop($foods); 
+
+    array_shift($foods); //remove first element and shift the array
+
+ 
+    $foods = array_reverse($foods); //this function returns a new array
+
+    //count the no of array elements
+    echo count($foods)."<br>";
+
+    // use for each loop to traverse array
+    foreach($foods as $food){
+        echo $food."<br>";
+    }
+?>
+
+
+
