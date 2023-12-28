06b384fa9080881646eeca11f252b20c2d7ed32f
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-28 18:57:22 +0530

    Update index.php

--- Include_function/index.php
+++ Include_function/index.php
@@ -1,1 +1,38 @@
 
+<?php
+
+include("header.html");
+/*
+     Include() - copies the content of a file php,text or html and includes it in your php file
+     Sections of the website become reusable
+     changes only to be made in one place
+
+     helps with code reusability
+     */
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    This is the home page <br>
+    All Home page info can ve shown here <br>
+</body>
+
+</html>
+
+<!-- Footer -->
+
+<?php
+    include("footer.html")
+
+?>
