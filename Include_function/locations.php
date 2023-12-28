48b479031e154c118e3163d89bc843bcfb2b5082
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-12-28 18:57:46 +0530

    Add files via upload

---
+++ Include_function/locations.php
@@ -0,0 +1,25 @@
+<?php
+include("header.html");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    This is the Locations page <br>
+    All Locations page info can ve shown here <br>
+</body>
+
+</html>
+
+
+<?php
+include("footer.html");
+?>
