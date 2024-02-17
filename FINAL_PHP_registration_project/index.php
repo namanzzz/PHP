8a99a2eb0a8ec7bb5c70711348199a07ae25a4ef
Naman <60322358+namanzzz@users.noreply.github.com>
2024-02-17 16:31:21 +0530

    Update index.php

--- FINAL_PHP_registration_project/index.php
+++ FINAL_PHP_registration_project/index.php
@@ -1,1 +1,64 @@
+<?php
+include("database.php");
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="<?php
+                    htmlspecialchars($_SERVER["PHP_SELF"])
+                    ?>
+        " method="post">
+        <h2>Wlcome to FakeBook</h2>
+        username: <br>
+        <input type="text" name="username">
+        <br>
+
+        password: <br>
+        <input type="password" name="password">
+        <br>
+
+        <input type="submit" name="submit" value="register">
+
+    </form>
+</body>
+
+</html>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username  = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password  = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if(empty($username)){
+        echo "Please Enter a username <br>";
+    }
+    else if(empty($password)){
+        echo "Please enter a password <br>";
+    }
+    else{
+
+        try{
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+            $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+            mysqli_query($conn, $sql);
+            echo "You have registered successfully!";
+
+        }catch(mysqli_sql_exception){
+            echo "Could not register user/ username is already taken!";
+        }
+
+
+    }
+}
+
+mysqli_close($conn);
+
+?>
