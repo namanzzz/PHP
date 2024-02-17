823c74cab34f2274975b98418acab806230fb2e6
Naman <60322358+namanzzz@users.noreply.github.com>
2024-02-17 13:22:02 +0530

    Create Impcomment.php

---
+++ php connect to mysql database/Impcomment.php
@@ -0,0 +1,17 @@
+<?php
+
+/**********  connecting to a mysql database
+ ***********************************/
+    
+
+    //  1. MySQLi Extension
+
+
+    // 2. PDO (php data objects)
+    // preferred over MySQLi because it can connect to several other databases such as PostGreSQL etc
+    // need to know object oriented programming
+
+
+    
+
+?>
