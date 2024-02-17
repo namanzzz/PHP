c11721d860d7d92b26164ed57ab3a8c6be976c18
Naman <60322358+namanzzz@users.noreply.github.com>
2024-02-17 16:31:42 +0530

    Create database.php

---
+++ FINAL_PHP_registration_project/database.php
@@ -0,0 +1,18 @@
+<?php
+
+    $db_server = "";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "businessdb";
+    $conn = "";
+
+    try{
+
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    // $conn -> object which represents our current connection
+    }
+    catch(mysqli_sql_exception){
+        echo "could not connect <br>";
+    }
+
+?>
