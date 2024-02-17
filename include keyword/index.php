dbf98a2c5425e19607eb0e553940606bd2908190
Naman <60322358+namanzzz@users.noreply.github.com>
2024-02-17 11:31:42 +0530

    Add files via upload

---
+++ include keyword/index.php
@@ -0,0 +1,25 @@
+
+<?php
+   include("header.html")
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is the home page <br>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi reiciendis temporibus libero nesciunt blanditiis harum delectus vero ipsam, unde dicta placeat quo eos non ipsum, veniam maiores ut alias iure.
+</body>
+</html>
+
+
+
+<?php
+    include("footer.html");
+
+?>
